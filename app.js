53a5ce3352dd25e9d3d4d0ccffec10f6f99f7d89
Harsh Kumar <harshkumar2009@gmail.com>
2020-03-06 15:50:46 +0530

    featch weather data from https

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ const express = require("express");
 const https = require("https")
 const app = express();
 
-app.get("/", function(req,res){
-    
+app.get("/", function (req, res) {
+
 })
+const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=87a67fe279e7933c7b2d841ed4012140&unit=metric"
 
+https.get(url, function (response) {
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+        console.log(data);
 
+        const weatherData = JSON.parse(data);
+        console.log(weatherData);
 
-app.listen(3000, function(){
+        const temp = weatherData.main.temp;
+        console.log("temperature = " + temp);
+
+        const description = weatherData.weather[0].description
+        console.log(description);
+        
+    })
+});
+
+app.listen(3000, function () {
     console.log("server is running on port 3000");
 });
