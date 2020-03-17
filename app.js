0c31832c048799cb88bd362538c4acb8750cb775
Harsh Kumar <harshkumar2009@gmail.com>
2020-03-17 22:45:40 +0530

    fix minor bug in icon url

--- app.js
+++ app.js
@@ -8,16 +8,14 @@ app.get("/", function (req, res) {
     https.get(url, function (response) {
         console.log(response.statusCode);
         response.on("data", function (data) {
-            
-
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon
-            const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            const imageUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
             res.write("<p>description of weather is </p>" + description);
             res.write("<h1>current temperature in london is " + temp + " degree conscious </h1>");
-            res.write("<img src=" + imageUrl + "/>");
+            res.write("<img src=" + imageUrl + ">");
             res.send();
         });
     });
